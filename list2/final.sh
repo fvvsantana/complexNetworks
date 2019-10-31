4da7817e3d95b8f204f636488286a1f221802990
Flavio Santana <fvvsantana@gmail.com>
2019-10-31 17:34:00 -0300

    Copy final.sh and readme.md from the list1

---
+++ list2/final.sh
@@ -0,0 +1,17 @@
+cp ex1/ex1_out.html final/
+cp ex2/ex2_out.html final/
+cp ex3/ex3_out.html final/
+cp ex4/ex4_out.html final/
+cp ex6/ex6_out.html final/
+cp ex7/ex7_out.html final/
+cp ex8/ex8_out.html final/
+cp ex9/ex9_out.html final/
+cp ex10/ex10_out.html final/
+
+cp readme.md final/
+
+zip -r final.zip final/
+
+
+
+
